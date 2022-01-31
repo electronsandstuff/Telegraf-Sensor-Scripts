15888e2400288abb61644d0f07c66143cfdc1c40
Christopher Pierce <chris@chris-pierce.com>
2022-01-30 21:13:30 -0500

    adding ccs811 back in

--- sparkfun_environmental_combo.py
+++ sparkfun_environmental_combo.py
@@ -1,16 +1,21 @@
 import json
 import time
 from patched_bm280_driver import QwiicBme280
-# from qwiic_ccs811 import QwiicCcs811
+from qwiic_ccs811 import QwiicCcs811
+
+
+# Some constants
+bme280_sleep_time = 0.5  # How long to wait after starting BME280
+ccs811_sleep_time = 2  # How long to wait after starting CCS811
 
 
 if __name__ == '__main__':
-    # Launch the drivers
+    # Launch the BME280 driver
     bme = QwiicBme280()
     if not bme.is_connected():
-        raise ValueError('BME sensor was not detected')
+        raise ValueError('BME280 sensor was not detected')
     bme.begin()
-    time.sleep(1)
+    time.sleep(bme280_sleep_time)
 
     # Get values from bme280
     sensor_vals = {
@@ -18,7 +23,22 @@ if __name__ == '__main__':
         'humidity': bme.read_humidity(),
         'pressure': bme.read_pressure(),
     }
-    bme.set_mode(bme.MODE_SLEEP)  # Put the BME280 to sleep until next time
+
+    # Launch the CCS811 driver
+    ccs = QwiicCcs811()
+    if not ccs.is_connected():
+        raise ValueError('CCS811 sensor was not detected')
+    err = ccs.begin()
+    if err != ccs.SENSOR_SUCCESS:
+        raise RuntimeError(f'CCS811 could not be initialized, begin returned {err}')
+    err = ccs.set_environmental_data(sensor_vals['humidity'], sensor_vals['temperature'])
+    if err != ccs.SENSOR_SUCCESS:
+        raise RuntimeError(f'Could not set CCS811 temp/humidity calibration, method returned {err}')
+    time.sleep(ccs811_sleep_time)
+
+    # Read sensor values
+    sensor_vals['tvoc'] = ccs.get_tvoc()
+    sensor_vals['co2_ccs811'] = ccs.get_co2()
 
     # Dump the data to STDOUT
     print(json.dumps(sensor_vals))
