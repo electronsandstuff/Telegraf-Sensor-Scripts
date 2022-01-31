2c8f1fed5980b9df1c50e5199b1b631847bc0e95
Christopher Pierce <chris@chris-pierce.com>
2022-01-30 19:39:42 -0500

    fixes methods for BME280, suppresses warnings for unused packages

--- sparkfun_environmental_combo.py
+++ sparkfun_environmental_combo.py
@@ -1,47 +1,31 @@
-from contextlib import contextmanager
-import sys, os
 import json
 import time
-
-
-@contextmanager
-def suppress_stdout():
-    with open(os.devnull, "w") as devnull:
-        old_stdout = sys.stdout
-        sys.stdout = devnull
-        try:
-            yield
-        finally:
-            sys.stdout = old_stdout
-
-
-# The qwiic library has some text output if optional packages arent' included. This suppresses it
-with suppress_stdout():
-    import qwiic
+from qwiic_bme280 import QwiicBme280
+from qwiic_ccs811 import QwiicCcs811
 
 
 if __name__ == '__main__':
     # Launch the drivers
-    bme = qwiic.QwiicBme280()
-    ccs = qwiic.QwiicCcs811()
+    bme = QwiicBme280()
 
     # Double check that the sensors are connected correctly
     if not bme.is_connected():
         raise ValueError('BME sensor was not detected')
-    if not ccs.is_connected():
-        raise ValueError('CCS811 sensor was not detected')
 
     # Start the drivers
     bme.begin()
-    ccs.begin()
-    ccs.read_algorithm_results()
+    bme.set_mode(bme.MODE_SLEEP)
     time.sleep(1)  # Give it a second before we read data
 
     # Read the data and dump to json output
+    bme.set_mode(bme.MODE_FORCED)  # Run one measurement
     print(json.dumps({
         'pressure': bme.read_pressure(),
         'humidity': bme.read_humidity(),
         'temp': bme.get_temperature_celsius(),
-        'tvoc': ccs.get_tvoc(),
-        'co2_ccs811': ccs.get_co2(),
+        #'tvoc': ccs.get_tvoc(),
+        #'co2_ccs811': ccs.get_co2(),
     }))
+
+    # Put BME280 to sleep
+    bme.set_mode(bme.MODE_SLEEP)
