020a4d25491893ff2bae5938a3135e38037192e5
Christopher Pierce <chris@chris-pierce.com>
2022-01-30 19:54:59 -0500

    still debugging BME280

--- sparkfun_environmental_combo.py
+++ sparkfun_environmental_combo.py
@@ -7,6 +7,7 @@ from qwiic_ccs811 import QwiicCcs811
 if __name__ == '__main__':
     # Launch the drivers
     bme = QwiicBme280()
+    bme.reset()
 
     # Double check that the sensors are connected correctly
     if not bme.is_connected():
@@ -14,11 +15,10 @@ if __name__ == '__main__':
 
     # Start the drivers
     bme.begin()
-    bme.set_mode(bme.MODE_SLEEP)
+    bme.set_mode(bme.MODE_NORMAL)
     time.sleep(1)  # Give it a second before we read data
 
     # Read the data and dump to json output
-    bme.set_mode(bme.MODE_FORCED)  # Run one measurement
     print(json.dumps({
         'pressure': bme.read_pressure(),
         'humidity': bme.read_humidity(),
